f1575e9203b81ffcf608121fe87fee6f6f231edd
Huy Hieu PHAM <hieuhuy01@gmail.com>
2017-09-27 00:06:55 +0200

    Create conditional_statement.py

---
+++ Python/conditional_statement.py
@@ -0,0 +1,36 @@
+# Huy-Hieu PHAM, Ph.D. Fellow.
+# Cerema Research Center & Toulouse Institute of Computer Science Research (IRIT)
+# Paul Sabatier University - Toulouse III, Toulouse, France.
+# Description: "Conditinal statement in Python".
+# Created date: Sep. 26, 2017
+
+# Input variable.
+x = 1
+
+# If statement.
+if x > 0:
+    print("x is positive")
+    
+# If/Else statement.
+if x > 0:
+    print("x is positive")
+    
+else:
+    print("X is 0 or negative")
+    
+# If/Elif/Else/
+
+if x > 0:
+    print("x is positive")
+
+elif:
+    print("x = 0")
+    
+else:
+    print("x is negative")
+    
+    
+
+    
+
+
