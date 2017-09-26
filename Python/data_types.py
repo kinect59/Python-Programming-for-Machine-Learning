ba6ef71b0c1185cd7107b69a65850d8dd64c9de7
Huy Hieu PHAM <hieuhuy01@gmail.com>
2017-09-26 11:50:42 +0200

    Create data_types.py

---
+++ Python/data_types.py
@@ -0,0 +1,9 @@
+# Huy-Hieu PHAM, Ph.D. Fellow.
+# Cerema Research Center & Toulouse Institute of Computer Science Research (IRIT)
+# Paul Sabatier University - Toulouse III, Toulouse, France.
+# Description: "Import module and packages in Python".
+# Created date: Sep. 26, 2017
+
+
+# Return the type of an object.
+type(2)
