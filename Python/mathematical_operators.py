a36cfadec9ccde22fce0e5a3b035f6fc14d5ebd7
Huy Hieu PHAM <hieuhuy01@gmail.com>
2017-09-26 14:18:52 +0200

    Create  mathematical_operators.py

---
+++ Python/mathematical_operators.py
@@ -0,0 +1,16 @@
+# Huy-Hieu PHAM, Ph.D. Fellow.
+# Cerema Research Center & Toulouse Institute of Computer Science Research (IRIT)
+# Paul Sabatier University - Toulouse III, Toulouse, France.
+# Description: "Mathematical Operators in Python".
+# Created date: Sep. 26, 2017
+
+# Basic mathematical operators.
+1 + 3                          # Add two numbers; return 4
+5 - 1                          # Subtract two numbers; return 4
+4 * 3                          # Multiply two numbers; return 12
+10 ** 2                        # Exponent; return 100
+10 / 4                         # Divide; it returns 2.5 
+10 // 4                        # Floor devision, returns 2.5
+10 % 3                         # Modulo, returns 1
+
+
