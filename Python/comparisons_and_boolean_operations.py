fecbf806c811a151587ea561be4137135093ec21
Huy Hieu PHAM <hieuhuy01@gmail.com>
2017-09-26 14:25:21 +0200

    Create comparisons_and_boolean_operations.py

---
+++ Python/comparisons_and_boolean_operations.py
@@ -0,0 +1,13 @@
+# Huy-Hieu PHAM, Ph.D. Fellow.
+# Cerema Research Center & Toulouse Institute of Computer Science Research (IRIT)
+# Paul Sabatier University - Toulouse III, Toulouse, France.
+# Description: "Comparisons and boolean operations in Python".
+# Created date: Sep. 26, 2017
+
+# Comparison two numbers and these returns.
+5 < 3                                     # Returns False
+5 > 3                                     # Returns True
+5 == 5                                    # Returns True
+5 != 2                                    # Returns True
+5 <= 2                                    # Returns False
+5 >= 2                                    # Returns True
