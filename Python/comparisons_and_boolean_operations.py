afd3f09a39a1aeada2346c3be658dd5478acaa52
Huy Hieu PHAM <hieuhuy01@gmail.com>
2017-09-26 23:59:25 +0200

    Update comparisons_and_boolean_operations.py

--- Python/comparisons_and_boolean_operations.py
+++ Python/comparisons_and_boolean_operations.py
@@ -11,3 +11,10 @@
 5 != 2                                    # Returns True
 5 <= 2                                    # Returns False
 5 >= 2                                    # Returns True
+
+# Boolean operations.
+5 > 3 and 4 > 2                           # Returns True
+5 > 3 or 1 < 0                            # Returns True
+
+
+
