b23aeec4a6953d670dba60729b9fcbe79404ebf0
Huy Hieu PHAM <hieuhuy01@gmail.com>
2018-05-10 17:39:54 +0200

    Update parse.py

--- Python/parse.py
+++ Python/parse.py
@@ -5,6 +5,8 @@ import cv2
 # construct the argument parse and parse the arguments to load an image with command-line.
 parser = argparse.ArgumentParser(description='Handwritten number recognition program using CNN.')
 parser.add_argument("-i", "--image", required=True, help="path to input image")
+parser.add_argument('--width', type=int, default=256, help="set image width")
+parser.add_argument('--length', type=int, default=256, help="set image length")
 args = vars(parser.parse_args())
 
 # load the image.
@@ -13,3 +15,5 @@ orig = image.copy()
 cv2.imshow("Output", image)
 cv2.waitKey(0)
 
+parser.add_argument('--length', type=int, default=10)
+parser.add_argument('--width', type=int, default=10.5)
