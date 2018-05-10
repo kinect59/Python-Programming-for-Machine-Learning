24be1b23c5523e5f189d38c3f66ac5e2b8509e49
Huy Hieu PHAM <hieuhuy01@gmail.com>
2018-05-10 17:15:16 +0200

    Update parse.py

--- Python/parse.py
+++ Python/parse.py
@@ -1,6 +1,15 @@
 import argparse
+import numpy as np
+import cv2
 
-parser = argparse.ArgumentParser('Program Description')
-parser.add_argument('--foo', help='foo help')
+# construct the argument parse and parse the arguments to load an image with command-line.
+parser = argparse.ArgumentParser(description='Handwritten number recognition program using CNN.')
+parser.add_argument("-i", "--image", required=True, help="path to input image")
+args = vars(parser.parse_args())
 
-args = parser.parse_args()
+# load the image.
+image = cv2.imread(args["image"])
+orig = image.copy()
+cv2.imshow("Output", image)
+cv2.waitKey(0)
+
