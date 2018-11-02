9ef519048ad8f74629dd166d7ae46cbbc996c56a
Huy Hieu PHAM <hieuhuy01@gmail.com>
2018-11-03 00:29:46 +0100

    Add files via upload

---
+++ Python/Preprocessing Images/install_OpenCV.py
@@ -0,0 +1,15 @@
+# Installing OpenCV
+
+"""
+
+Using unofficial pre-built OpenCV packages for Python via:
+
+pip install opencv-python
+
+"""
+
+# Load library
+import cv2
+
+# Check version
+print(cv2.__version__)
